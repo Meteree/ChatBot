b99e82d2f3d27be3d1d6db25fda5485b4ae8a31a
Meteree <mete06alp06cetin@gmail.com>
2025-03-18 19:58:11 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,21 @@
+import discord
+from config import token
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f' {client.user} sunucuya atmosferi delerek iniş yaptı')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('hello'):
+        await message.channel.send('Hello!')
+
+client.run(token)
